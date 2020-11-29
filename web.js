126c8cdf507dc208072bcfbe430a605796cee7b9
Medvedinca <dan.blinov@yandex.ru>
2020-11-30 05:18:55 +0700

    использование node.js для статики

--- web.js
+++ web.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 app.use(express.static(path.join(process.cwd(), 'src')));
-console.log(process.env.PORT || 5000);
-app.listen(process.env.PORT || 5000);
+http.listen((process.env.PORT || 5000), function(){
+  console.log('listening on *:5000');
+});
