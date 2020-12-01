661c1eebccf5c70024fdf00fc633b8eb6f785a0f
Medvedinca <dan.blinov@yandex.ru>
2020-12-01 20:36:43 +0700

    Update

--- index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-const PORT = process.env.PORT || '8080';
-var app = express();
-app.set("port", PORT);
-var path = require('path');
-app.use(express.static(path.join(process.cwd(), 'src')));
